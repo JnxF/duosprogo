3ad2eb84ed86565cebb1f79f18b5f3b1c0a820ef
Francisco Martínez Lasaca <frml@itu.dk>
2021-03-11 16:12:41 +0100

    :art: Prettify GuessMeaning and TranslateSentence

--- src/components/exercises/GuessMeaning.tsx
+++ src/components/exercises/GuessMeaning.tsx
@@ -1,28 +1,48 @@
-import { Alert, Button, HStack, Spacer } from "@chakra-ui/react";
+import { Alert, Box, Button, HStack, Spacer } from "@chakra-ui/react";
 import React from "react";
 import SentenceDisplayer from "../helpers/SentenceDisplayer";
-import { generalState } from "../state";
+import { generalState, States } from "../state";
 import GeneralExercise from "./GeneralExercise";
 
 export default function GuessMeaning() {
-    const danish = generalState(state => state.currentTranslation?.english);
-    const english = generalState(state => state.currentTranslation?.danish);
+    const state = generalState(_ => _.gameState);
+    const danish = generalState(_ => _.currentTranslation?.danish);
 
     return (
         <GeneralExercise title="Forstår du det?">
-            <SentenceDisplayer isDanish={true}>{english ?? ""}</SentenceDisplayer>
-            <HStack mt={8}>
-                <Spacer />
-                <Button size="lg" colorScheme="blue" onClick={generalState(state => state.checkAnswer)}>Vis</Button>
-            </HStack>
+            <SentenceDisplayer isDanish={true}>{danish ?? ""}</SentenceDisplayer>
+            {state === States.RunningExercise && <ShowAnswerButton />}
+            {state === States.CheckingAnswer && <RightOrWrong />}
+        </GeneralExercise >
+    )
+}
+
+function ShowAnswerButton() {
+    const checkAnswer = generalState(_ => _.checkAnswer);
+
+    return (
+        <HStack mt={8}>
+            <Spacer />
+            <Button size="lg" colorScheme="blue" onClick={checkAnswer}>Vis</Button>
+        </HStack>
+    )
+}
+
+function RightOrWrong() {
+    const english = generalState(_ => _.currentTranslation?.english);
+    const addNegative = generalState(_ => _.addNegative);
+    const addPositive = generalState(_ => _.addPositive);
+
+    return (
+        <Box>
             <Alert status="info" variant="subtle" mt={"8"} p={5}>
-                <SentenceDisplayer isDanish={false}>{danish ?? ""}</SentenceDisplayer>
+                <SentenceDisplayer isDanish={false}>{english ?? ""}</SentenceDisplayer>
             </Alert>
             <HStack mt={8}>
                 <Spacer />
-                <Button size="lg" colorScheme="red" onClick={generalState(state => state.addNegative)}>Jeg gjorde ikke</Button>
-                <Button size="lg" colorScheme="green" onClick={generalState(state => state.addPositive)}>Jeg gjorde</Button>
+                <Button size="lg" colorScheme="red" onClick={addNegative}>Jeg gjorde ikke</Button>
+                <Button size="lg" colorScheme="green" onClick={addPositive}>Jeg gjorde</Button>
             </HStack>
-        </GeneralExercise >
+        </Box>
     )
 }
--- src/components/exercises/TranslateSentence.tsx
+++ src/components/exercises/TranslateSentence.tsx
@@ -1,35 +1,60 @@
 import { ArrowForwardIcon } from "@chakra-ui/icons";
-import { Alert, AlertIcon, AlertTitle, Button, HStack, Spacer, Textarea } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Button, HStack, Spacer, Textarea } from "@chakra-ui/react";
 import React from "react";
 import SentenceDisplayer from "../helpers/SentenceDisplayer";
 import { generalState, States } from "../state";
 import GeneralExercise from "./GeneralExercise";
 
 export default function TranslateSentence() {
-    const danish = generalState(state => state.currentTranslation?.danish);
-
     const gameState = generalState(state => state.gameState);
-    const alert = gameState === States.CheckingAnswer ? (
+
+    const danish = generalState(state => state.currentTranslation?.danish);
+    const english = generalState(state => state.currentTranslation?.english);
+
+    return (
+        <GeneralExercise title="Oversæt denne sætning">
+            <SentenceDisplayer isDanish={true}>{danish ?? ""}</SentenceDisplayer>
+            <Textarea border={"2px"} resize="none" isDisabled={gameState === States.CheckingAnswer} data-gramm_editor="false" autoComplete="false" autoFocus fontSize="xl" mt={8} />
+            <NextStep />
+            {gameState === States.CheckingAnswer && <Feedback />}
+        </GeneralExercise>
+    )
+}
+
+function NextStep() {
+    const gameState = generalState(state => state.gameState);
+
+    return (
+        <HStack mt={5}>
+            <Spacer />
+            <Button size="lg" colorScheme="blue" onClick={gameState === States.RunningExercise ? generalState(state => state.checkAnswer) : generalState(state => state.addPositive)} rightIcon={gameState === States.CheckingAnswer ? <ArrowForwardIcon /> : undefined}>
+                {gameState === States.RunningExercise ? "Tjek" : "Fortsæt"}
+            </Button>
+        </HStack>
+    )
+}
+
+function Feedback() {
+    const english = generalState(state => state.currentTranslation?.english);
+
+    return (
         <Alert status="success" variant="subtle" mt={"8"} p={5}>
             <AlertIcon boxSize="40px" mr={0} />
             <AlertTitle mt={1} mb={1} ml={5} fontSize="xl">
                 Du er ret!
             </AlertTitle>
         </Alert>
-    ) : <></>;
+    )
 
-    return (
-        <GeneralExercise title="Oversæt denne sætning">
-            <SentenceDisplayer isDanish={true}>{danish ?? ""}</SentenceDisplayer>
-            <Textarea resize="none" isDisabled={gameState === States.CheckingAnswer} data-gramm_editor="false" autoComplete="false" autoFocus fontSize="xl" mt={8} />
-
-            <HStack mt={5}>
-                <Spacer />
-                <Button size="lg" colorScheme="blue" onClick={gameState === States.RunningExercise ? generalState(state => state.checkAnswer) : generalState(state => state.addPositive)} rightIcon={gameState === States.CheckingAnswer ? <ArrowForwardIcon /> : undefined}>
-                    {gameState === States.RunningExercise ? "Tjek" : "Fortsæt"}
-                </Button>
-            </HStack>
-            {alert}
-        </GeneralExercise>
+    const incorrectAlert = (
+        <Alert status="error" variant="subtle" mt={"8"} px={5} pt={5} pb={8}>
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={1} mb={1} ml={5} fontSize="xl">
+                Oops!
+            </AlertTitle>
+            <AlertDescription ml={5}>Det rigtige svar er:
+                <SentenceDisplayer isDanish={false}>{english ?? ""}</SentenceDisplayer>
+            </AlertDescription>
+        </Alert>
     )
 }
