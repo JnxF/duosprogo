96f0febbb2c65a329e3f264cedeafb956e3807ff
Francisco Martínez Lasaca <frml@itu.dk>
2021-03-13 01:43:44 +0100

    :recycle: Refactor Levenshtein function

--- src/state/state.tsx
+++ src/state/state.tsx
@@ -3,6 +3,7 @@ import { data, Translation } from '../Data';
 import shuffle from '../utils/shuffle';
 import { AnswerScore } from './AnswerScore';
 import { ExerciseType } from './ExerciseType';
+import { levenshtein } from './levenshtein';
 import { States } from './States';
 
 export type GameState = {
@@ -95,29 +96,6 @@ export const generalState = create<GameState>((set: SetState<GameState>, get: Ge
         const { currentTranslation, exerciseType } = get();
 
         if (exerciseType === ExerciseType.TranslateSentenceExercise) {
-
-            const levenshtein = (a: string, b: string): number => {
-                if (a === "" || b === "") return 50;
-
-                const maxSize = Math.max(a.length, b.length);
-                a = a.padStart(maxSize, " ")
-                b = b.padStart(maxSize, " ")
-
-                const matrix = Array.from({ length: a.length })
-                    .map(() => Array.from({ length: b.length })
-                        .map(() => 0))
-                for (let i = 0; i < a.length; i++) matrix[i][0] = i
-                for (let i = 0; i < b.length; i++) matrix[0][i] = i
-                for (let j = 0; j < b.length; j++)
-                    for (let i = 0; i < a.length; i++)
-                        matrix[i][j] = Math.min(
-                            (i === 0 ? 0 : matrix[i - 1][j]) + 1,
-                            (j === 0 ? 0 : matrix[i][j - 1]) + 1,
-                            (i === 0 || j === 0 ? 0 : matrix[i - 1][j - 1]) + (a[i] === b[j] ? 0 : 1)
-                        )
-                return matrix[a.length - 1][b.length - 1]
-            }
-
             const normalize = (input: string) => {
                 let res = input.trim();
                 res = res.toLowerCase();
