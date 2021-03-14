d9c327e0a36573a8eaad75a47943933ccd0b8dcc
Francisco Martínez Lasaca <frml@itu.dk>
2021-03-14 16:19:37 +0100

    Favicons (#3)
    * :lipstick: Create favicon
    
    * :sparkles: Indeterminate state for modules

--- src/components/gui/Modules.tsx
+++ src/components/gui/Modules.tsx
@@ -1,39 +1,59 @@
-import { Button } from "@chakra-ui/button";
-import { useDisclosure } from "@chakra-ui/hooks";
-import { ArrowForwardIcon } from "@chakra-ui/icons";
 import { Box, Checkbox, Heading, Stack } from "@chakra-ui/react";
 import { organizedTranslations } from "../../Data";
 import { generalState } from "../../state/state";
 import { GlosarModal } from "./GlosarModal";
 
 export default function Modules() {
-    const { isOpen, onOpen, onClose } = useDisclosure()
-    const activated = generalState(_ => _.modulsActivated);
-    const toggleCategory = generalState(_ => _.toggleCategory);
+    // Moduls hooks
+    const modulsActivated = generalState(_ => _.modulsActivated);
+    const setCategory = generalState(_ => _.setCategory);
     const selectedCategories = generalState(_ => _.selectedCategories);
 
     return <>
-        {Object.entries(organizedTranslations).map(([modul, categories]) => (
-            <Box mb="5" key={modul}>
-                <Heading fontSize="md" mb="2">Modul {modul}</Heading>
-                <Stack align={"left"} pl={{ xs: 2, md: 3 }} borderLeft={"2px"} borderColor={"gray.300"} spacing={{ sm: 0, md: 1 }}>
-                    {Object.entries(categories).map(([category]) =>
-                        <Checkbox
-                            isDisabled={!activated}
-                            size="sm"
-                            pl={2}
-                            key={category}
-                            {...selectedCategories.includes(category) ? { "defaultIsChecked": true } : undefined}
-                            onChange={(e) => toggleCategory(category)}
-                        >
-                            {category.replace(/_/g, " ")}
-                        </Checkbox>
-                    )}
-                </Stack>
-            </Box>
-        ))
+        {
+            Object.entries(organizedTranslations).map(([modul, categories], idx1) => {
+                const selectedCategoriesOfModul = Object.entries(categories).filter(([c]) => selectedCategories.includes(c));
+                const isIndeterminate = selectedCategoriesOfModul.length !== 0 && selectedCategoriesOfModul.length !== Object.entries(categories).length;
+                return (
+                    <Box mb="5" key={idx1}>
+                        <Heading fontSize="md" mb="2">
+                            <Checkbox
+                                defaultChecked={true}
+                                isDisabled={!modulsActivated}
+                                isIndeterminate={isIndeterminate}
+                                size="md"
+                                ml={0}
+                                value={modul}
+                                onClick={(e) => {
+                                    Object.entries(categories).forEach(([cat]) => {
+                                        setCategory(cat, (e.target as HTMLInputElement).checked);
+                                    })
+                                }}
+                            >
+                                Modul {modul}
+                            </Checkbox>
+                        </Heading>
+                        <Stack align={"left"} ml={1} pl={1} borderLeft={"2px"} borderColor={"gray.300"} spacing={{ sm: 0, md: 1 }}>
+                            {Object.entries(categories).map(([category], idx2) =>
+                                <Checkbox
+                                    isDisabled={!modulsActivated}
+                                    isChecked={selectedCategories.includes(category)}
+                                    size="sm"
+                                    pl={2}
+                                    key={idx2}
+                                    onChange={(e) => setCategory(
+                                        category,
+                                        (e.target as HTMLInputElement).checked
+                                    )}
+                                >
+                                    {category.replace(/_/g, " ")}
+                                </Checkbox>
+                            )}
+                        </Stack>
+                    </Box>
+                )
+            })
         }
-        <Button size="sm" onClick={onOpen} rightIcon={<ArrowForwardIcon />} colorScheme="blue" variant="outline">Viser glosar</Button>
-        <GlosarModal isOpen={isOpen} onClose={onClose} />
+        <GlosarModal />
     </>
 }
